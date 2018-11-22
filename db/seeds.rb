fbb63f1b0e2877278478f560611f0d2ae33b95cc
Damien Delahaye <damien.delahaye2@gmail.com>
2018-11-22 15:09:21 +0100

    exo2_routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "peach")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "strawberries")
+Ingredient.create(name: "curacao")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "cranberries")
