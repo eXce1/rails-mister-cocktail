cfd8e3146de23bd0aa23364ddc87d60d1c15e7e0
Damien Delahaye <damien.delahaye2@gmail.com>
2018-11-22 17:26:46 +0100

    exo2_advanced

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "peach")
-Ingredient.create(name: "soda")
-Ingredient.create(name: "strawberries")
-Ingredient.create(name: "curacao")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "cranberries")
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+Ingredient.create!(name: "peach")
+Ingredient.create!(name: "soda")
+Ingredient.create!(name: "strawberries")
+Ingredient.create!(name: "curacao")
+Ingredient.create!(name: "vodka")
+Ingredient.create!(name: "cranberries")
+
+Cocktail.create!(name: "TGV")
+Cocktail.create!(name: "Trashed")
+Cocktail.create!(name: "L'alcoolique anonyme")
+
+Dose.create!(
+  description: 'Only for the noobs',
+  cocktail_id: Cocktail.find_by(name: "TGV").id,
+  ingredient_id: Ingredient.find_by(name: "lemon").id
+)
+Dose.create!(
+  description: 'Maximum alcool',
+  cocktail_id: Cocktail.find_by(name: "Trashed").id,
+  ingredient_id: Ingredient.find_by(name: "soda").id
+)
+
+Dose.create!(
+  description: 'Explosing head',
+  cocktail_id: Cocktail.find_by(name: "L'alcoolique anonyme").id,
+  ingredient_id: Ingredient.find_by(name: "ice").id
+)
+
+
